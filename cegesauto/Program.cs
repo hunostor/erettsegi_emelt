16e490b8867529de87a90b5ab704fd1ff8edeb76
Attila Poroszkai <poroszkai.attila@gmail.com>
2020-03-20 19:35:41 +0100

    first commit

---
+++ cegesauto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cegesauto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
